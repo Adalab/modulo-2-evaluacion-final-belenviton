98ffe9273df96f2c499247bbe75040a17ee790e9
belenviton <belenviton@hotmail.com>
2024-01-10 10:53:33 +0100

    add list of favourites and implemented botton search

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ const favouritesList = document.querySelector('.js__favourites_list');
 //VARIABLES
 let disneyCharacters = [];
 let favouriteCharacters = [];
-let completLi = principalList.children;
+let selectedCharacter = [];
 
 //let allList = completLi.children;
 
@@ -25,6 +25,15 @@ const createList = (character) => {
   const imgCharacters = document.createElement('img');
   imgCharacters.classList.add('card-image');
   imgCharacters.src = `${character.imageUrl}`;
+
+  /*const printImage = disneyCharacters.findIndex((character) => {
+    if (character.imageUrl === -1) {
+      imgCharacters.src = `https://via.placeholder.com/200x170/ff/666/?text=${character.name}`;
+    } else {
+      
+    }
+  });*/
+
   imgCharacters.alt = `${character.name}`;
   imgCharacters.title = `Add character to favourites`;
   //crear h3
@@ -39,22 +48,23 @@ const createList = (character) => {
   liCharacters.addEventListener('click', handleClickCard);
 };
 
-const createFavourites = (character) => {
+const createFavourites = (description) => {
   //crear li
   const liCharacters = document.createElement('li');
 
-  liCharacters.classList.add('favourites');
-  liCharacters.setAttribute('data-ident', `${character._id}`);
+  liCharacters.classList.add('favourites-cards');
+  liCharacters.setAttribute('data-ident', `${description._id}`);
 
   //crear img
   const imgCharacters = document.createElement('img');
   imgCharacters.classList.add('card-image');
-  imgCharacters.src = `${character.imageUrl}`;
-  imgCharacters.alt = `${character.name}`;
-  imgCharacters.title = `Add character to favourites`;
+
+  imgCharacters.src = `${description.imageUrl}`;
+  imgCharacters.alt = `${description.name}`;
+  imgCharacters.title = `Favourite-card`;
   //crear h3
   const nameCharacters = document.createElement('h3');
-  const cardName = document.createTextNode(`${character.name}`);
+  const cardName = document.createTextNode(`${description.name}`);
   nameCharacters.appendChild(cardName);
   nameCharacters.classList.add('card-name');
   //UL
@@ -64,50 +74,65 @@ const createFavourites = (character) => {
 };
 
 const renderAllCharacters = () => {
+  principalList.innerHTML = '';
   for (const character of disneyCharacters) {
     createList(character);
   }
 };
 
 const renderFavourites = () => {
-  for (const character of favouriteCharacters) {
-    createFavourites(character);
+  favouritesList.innerHTML = '';
+  for (const description of favouriteCharacters) {
+    createFavourites(description);
   }
 };
+
 // FUNCIONES HANDLE
 
 const handleClickCard = (event) => {
   const oneCharacter = event.currentTarget;
   oneCharacter.classList.toggle('favourites');
-  favouriteCharacters.push(oneCharacter);
-  renderFavourites();
 
+  const oneClickedCharacter = parseInt(oneCharacter.dataset.ident);
+  const oneFavourite = disneyCharacters.find(
+    (oneObjectCharacter) => oneObjectCharacter._id === oneClickedCharacter
+  );
+  favouriteCharacters.push(oneFavourite);
+  renderFavourites();
+  console.log(oneClickedCharacter);
+  console.log(disneyCharacters);
   console.log(oneCharacter);
+  console.log(favouriteCharacters);
+  console.log(oneFavourite);
+};
+
+const handleClickSearch = () => {
+  let inputValue = iptText.value;
+  let inputTextPrint = inputValue.replace(/ /g, '%20');
+  fetch(`//api.disneyapi.dev/character?name=${inputTextPrint}`)
+    .then((response) => response.json())
+    .then((data) => {
+      disneyCharacters = data.data;
+
+      renderAllCharacters();
+    });
 };
 //EVENTOS
 formHeader.addEventListener('submit', (event) => {
   event.preventDefault();
 });
 
+inputButton.addEventListener('click', handleClickSearch);
+
 //AL CARGAR LA PÁGINA
 fetch('//api.disneyapi.dev/character?pageSize=50')
   .then((response) => response.json())
   .then((data) => {
     disneyCharacters = data.data;
-    favouriteCharacters = data.data;
     renderAllCharacters();
-    console.log(disneyCharacters);
+    disneyCharacters.indexOf((images) => {
+      if (images.imageUrl === -1) {
+        imgCharacters.src = `https://via.placeholder.com/200x170/ff/666/?text=${character.name}`;
+      }
+    });
   });
-
-//siguiente paso
-
-/* const handleClickSearch = () => { 
-
-fetch('//api.disneyapi.dev/character')
-.then((response) => response.json())
-.then((data) => {
-  selectedCharacter = data.name
-  selectedCharacter = api.disneyapi.dev/character?name=${iptText.value}
-  renderOnlySearch();
-})};
-inputButton.addEventListener('click', handleClickSearch); */
